e8d5d4be779aefc69d0596e07bb859dd16eb6a48
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-21 15:26:31 -0700

    Practice all 3  scenarios of deletion

--- Learning/Circular-LinkedList-Deletion/Circular-LinkedList-Deletion.c
+++ Learning/Circular-LinkedList-Deletion/Circular-LinkedList-Deletion.c
@@ -20,6 +20,50 @@ void circularLinkedListTraversal(struct Node *head)
     printf("Element: %d\n", ptr->data);
 }
 
+struct Node *deleteFirst(struct Node *head)
+{
+    struct Node *ptr1 = head->next;
+    struct Node *ptr2 = head;
+
+    do
+    {
+        ptr2 = ptr2->next;
+
+    } while (ptr2->next != head);
+
+    ptr2->next = ptr1;
+
+    return ptr1;
+}
+
+void deletionAtIndex(struct Node *head, int index)
+{
+
+    struct Node *ptr = head;
+    struct Node *ptr2 = head->next;
+    int i = 0;
+
+    while (i < index - 2)
+    {
+        ptr = ptr->next;
+        ptr2 = ptr2->next;
+        i++;
+    }
+
+    ptr->next = ptr2->next;
+}
+
+void deleteAtLast(struct Node *head)
+{
+    struct Node *ptr = head->next;
+    struct Node *ptr2 = head;
+    while (ptr->next != head)
+    {
+        ptr = ptr->next;
+        ptr2 = ptr2->next;
+    }
+    ptr2->next = ptr->next;
+}
 int main(int argc, char const *argv[])
 {
     struct Node *head = (struct Node *)malloc(sizeof(struct Node));
@@ -39,8 +83,19 @@ int main(int argc, char const *argv[])
     fourth->data = 4;
     fourth->next = head;
 
-    printf("Before Insertion\n");
+    printf("Before Deletion\n");
     circularLinkedListTraversal(head);
 
+    printf("After Deletion First Node\n");
+    head = deleteFirst(head);
+    circularLinkedListTraversal(head);
+
+    // printf("After Deletion At Index\n");
+    // deletionAtIndex(head, 2);
+    // circularLinkedListTraversal(head);
+
+    printf("After Deletion Last Node\n");
+    deleteAtLast(head);
+    circularLinkedListTraversal(head);
     return 0;
 }
