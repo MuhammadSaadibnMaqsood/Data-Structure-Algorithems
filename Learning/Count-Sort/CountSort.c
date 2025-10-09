e3910aacfa9a0304fe4facc0c5d5a6f16fd88257
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-10-09 15:03:39 -0700

    Practicing Count sort

---
+++ Learning/Count-Sort/CountSort.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+void traversal(int arr[], int n)
+{
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+int maximum(int arr[], int n)
+{
+    int max = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+void CountSort(int arr[], int n)
+{
+    int i, j;
+
+    int max = maximum(arr, n);
+
+    int *count = (int *)malloc(max + 1 * sizeof(int));
+
+    for (i = 0; i <= max; i++)
+    {
+        count[i] = 0;
+    }
+    for (i = 0; i < n; i++)
+    {
+        count[arr[i]] = +1;
+    }
+    i = 0;
+    j = 0;
+
+    while (i <= max)
+    {
+        if (count[i] > 0)
+        {
+            arr[j] = count[i];
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+int main()
+{
+    int arr[] = {2, 4, 7, 1, 3, 5, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("\nBefore sorting:");
+    traversal(arr, n);
+
+    CountSort(arr, n - 1);
+
+    printf("\nAfter sorting:");
+    traversal(arr, n);
+
+    return 0;
+}
