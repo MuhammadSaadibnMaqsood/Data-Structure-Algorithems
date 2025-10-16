3f92f602e593bf98448c7dacb7e610460bc0fed8
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-10-15 22:47:55 -0700

    added BST CHECKING

--- Learning/BST-Checking/BST_Checking.c
+++ Learning/BST-Checking/BST_Checking.c
@@ -8,9 +8,27 @@ struct Node
     struct Node *right;
 };
 
+struct Node *prev = NULL;
+
+int isBST(struct Node *root)
+{
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+            return 0;
+
+        if (prev != NULL && root->data <= prev->data)
+            return 0;
+
+        prev = root;
+
+        return isBST(root->right);
+    }
+    return 1;
+}
+
 void inOrderTraversal(struct Node *root)
 {
-
     if (root)
     {
         inOrderTraversal(root->left);
@@ -19,71 +37,39 @@ void inOrderTraversal(struct Node *root)
     }
 }
 
-int isBST(struct Node *root)
-{
-    if (root)
-    {
-        if (root->left < root->data && root->right > root->data)
-        {
-            isBST(root->left);
-            isBST(root->right);
-        }
-        else
-        {
-            return 0;
-        }
-
-        return 1;
-    }
-
-    return 1;
-}
-
 struct Node *createNode(int data)
 {
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->left = NULL;
     newNode->right = NULL;
-
     return newNode;
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
-    // ROOT NODE
     struct Node *n1 = createNode(11);
-
-    // SECOND & THIRD CHILD
     struct Node *n2 = createNode(9);
     struct Node *n3 = createNode(13);
-
-    n1->left = n2;
-    n1->right = n3;
-
-    // FOUTH & FIFTH CHILD
     struct Node *n4 = createNode(8);
     struct Node *n5 = createNode(10);
-
-    n2->left = n4;
-    n2->right = n5;
-
-    // SIXTH & SEVENTH CHILD
     struct Node *n6 = createNode(12);
     struct Node *n7 = createNode(15);
 
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    n2->right = n5;
     n3->left = n6;
     n3->right = n7;
 
+    printf("Inorder Traversal: ");
     inOrderTraversal(n1);
+
     if (isBST(n1))
-    {
-        printf("Tree is BST");
-    }
+        printf("\nTree is BST");
     else
-    {
-        printf("Tree is not BST");
-    }
+        printf("\nTree is not BST");
 
     return 0;
 }
