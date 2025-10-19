d7a013d143496f3c1053b6e4dce9f21083a38ed3
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-10-18 21:47:16 -0700

    Practicing Insertion in binary Tree

---
+++ Learning/Binary-Tree-Insertion/insertion.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int main()
+{
+    struct Node *n1 = createNode(11);
+    struct Node *n2 = createNode(9);
+    struct Node *n3 = createNode(13);
+    struct Node *n4 = createNode(8);
+    struct Node *n5 = createNode(10);
+    struct Node *n6 = createNode(12);
+    struct Node *n7 = createNode(15);
+
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    n2->right = n5;
+    n3->left = n6;
+    n3->right = n7;
+}
