0a22ece5718ad3736059e5900c5adceecd89d9eb
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-10-18 21:14:55 -0700

    Added Binary Searching

--- Learning/Binary-Tree-Searching/Searching.c
+++ Learning/Binary-Tree-Searching/Searching.c
@@ -1,5 +1,70 @@
-int main(int argc, char const *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
 {
-    /* code */
-    return 0;
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node *search(struct Node *root, int el)
+{
+
+    if (root == NULL)
+    {
+        return 0;
+    }
+    if (root->data == el)
+    {
+        return root;
+    }
+
+    if (el < root->data)
+    {
+       
+        return search(root->left, el);
+    }
+    else
+    {
+        return search(root->right, el);
+    }
+}
+
+int main()
+{
+    struct Node *n1 = createNode(11);
+    struct Node *n2 = createNode(9);
+    struct Node *n3 = createNode(13);
+    struct Node *n4 = createNode(8);
+    struct Node *n5 = createNode(10);
+    struct Node *n6 = createNode(12);
+    struct Node *n7 = createNode(15);
+
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    n2->right = n5;
+    n3->left = n6;
+    n3->right = n7;
+
+    struct Node *searchEl = search(n1, 15);
+
+    if (searchEl)
+    {
+        printf("\nFound: %d", searchEl->data);
+    }
+    else
+    {
+        printf("\nNot found");
+    }
 }
