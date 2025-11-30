9580a791011e5b1e64aa3dd0ca5b87fb848dc7ba
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-11-30 10:13:39 -0800

    Added 25th problem

---
+++ Leet-Code/25th-Problem/unique_character.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int front;
+    int rear;
+    int arr[100000];
+} Queue;
+
+void push(Queue *q, int value)
+{
+    q->arr[q->rear++] = value;
+}
+
+void pop(Queue *q)
+{
+    q->front++;
+}
+
+int front(Queue *q)
+{
+    return q->arr[q->front];
+}
+
+int empty(Queue *q)
+{
+    return q->front == q->rear;
+}
+
+int firstUniqChar(char *s)
+{
+    int freq[26] = {0};
+
+    Queue q = {.front = 0, .rear = 0};
+
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        freq[s[i] - 'a']++;
+        push(&q, i);
+    }
+
+    while (!empty(&q))
+    {
+        int idx = front(&q);
+        if (freq[s[idx] - 'a'] == 1)
+        {
+            return idx;
+        }
+        pop(&q);
+    }
+    return -1;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
