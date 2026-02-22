0064868a8eee7c354e83b0dd85f10d422b9dd98e
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-02-22 11:23:25 -0800

    Added 49th-Problem

---
+++ Leet-Code/49th-Problem/find_next.c
@@ -0,0 +1,15 @@
+struct Node* connect(struct Node* root) {
+    
+    if (!root || !root->left)
+        return root;
+
+    root->left->next = root->right;
+
+    if (root->next)
+        root->right->next = root->next->left;
+
+    connect(root->left);
+    connect(root->right);
+
+    return root;
+}
